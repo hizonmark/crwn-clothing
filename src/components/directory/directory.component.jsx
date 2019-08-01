ec626b86b6e013ba803b13ba7d5dd6236c838cbd
Mark John Hizon <hizonmark3@google.com>
2019-08-01 12:32:12 +0800

    added routing via react-router-dom and made changes to components to be able to rout dynamically based on state of directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,13 +47,7 @@ class Directory extends React.Component {
         return <div className="directory-menu">
             {
                 sections.map(
-                    ({ title, id, imageUrl, size }) =>
-                        <MenuItem
-                            key={id}
-                            title={title}
-                            imageUrl={imageUrl}
-                            size={size}
-                        />
+                    ({ id, ...otherProps }) => <MenuItem key={id} {...otherProps} />
                 )
             }
         </div>
