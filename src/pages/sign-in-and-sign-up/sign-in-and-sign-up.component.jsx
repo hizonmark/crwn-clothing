3fff793e43b15af53ad05c804e542ac491a34712
Mark John Hizon <hizonmark3@google.com>
2019-08-02 19:14:31 +0800

    added sign-in-and-sign-up page together with components sign-in form, form-input and custom-button also added styling for each

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss'
+import SignInForm from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+
+    <div className="sign-in-and-sign-up">
+        <SignInForm />
+    </div>
+);
+
+export default SignInAndSignUpPage;
