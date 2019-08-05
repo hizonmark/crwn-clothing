7311e56c2fca803eee5d76d51995b1a4f090dd40
Mark John Hizon <hizonmark3@google.com>
2019-08-05 16:11:45 +0800

    refactored uesr reducer and user action to get the action type string from user types

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -45,7 +45,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" render={
+            () => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)
+          } />
         </Switch>
       </div>
     );
@@ -53,8 +55,12 @@ class App extends React.Component {
 
 }
 
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
